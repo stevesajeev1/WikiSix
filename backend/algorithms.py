a5f2602d6ca574782a690b3c2b2007f330cd18f6
sriramyy <131324693+sriramyy@users.noreply.github.com>
2025-04-15 19:31:22 -0400

    Created the Graph class and implemented function to create and Adjacency List from a file input

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -2,6 +2,58 @@
 # Each algorithm should accept a graph object, start node and end node.
 # It should return the number of nodes explored and ALL the shortest paths.
 
-# def dijkstra(graph, start, end):
-# def bfs(graph, start, end):
-# ...
+# Imports
+from graph import Graph
+import heapq
+
+
+# Algorithm Functions
+# Should take in src, to
+# Should output number of nodes explored and shortest path
+
+# Dijkstra Algorithm - not working rn
+def dijkstra(graph:Graph, src, to):
+
+    # get the number of nodes in the graph
+    n = graph.get_num_nodes()
+
+    # initialize the distances
+    INF = float('inf')
+    dist = [INF] * n
+    dist[src] = 0
+
+    # initialize visited set
+    visited = set()
+
+    # priority queue to hold (node, distance)
+    pq = [(src, 0)]
+
+    # process until empty
+    while pq:
+        node = heapq.heappop(pq)
+
+        # relax all of node's edges
+        for v in graph.get_neighbors(node):
+            weight = 1
+            # node allows a smaller weight and v is not visited
+            if (dist[v] > dist[node] + weight) & (v not in visited) :
+                # replace the weight, append to pq, add to visited
+                dist[v] = dist[node] + weight
+                pq.append((v,weight))
+                visited.add(v)
+            
+                if v is to:
+                    # found the destination (to) node
+                    break
+    
+    return len(visited), dist[to]
+
+
+# breadth first search
+def bfs (graph:Graph, src, to):
+    pass
+
+
+# depth first search
+def dfs (graph:Graph, src, to):
+    pass
--- backend/graph.py
+++ backend/graph.py
@@ -1,7 +1,65 @@
 # Class for adjacency list reprsentation of data
-# Most likely will need a TSV reader package
+# TODO: include a text file that just has the connections between sites
 
-# class Graph:
-#   __init__(filename)
-#   get_edges()
-#   (other methods)...
+class Graph:
+
+#-- Initialize the graph using the file
+    def __init__(self, filename):
+        self._adjList = {}
+        self._numEdges = 0
+        self._numNodes = 0
+        self.loadGraph(filename)
+
+
+#-- Method to load the graph from the file
+    def loadGraph(self, filename):
+        # open the file for reading
+        file = open(filename, 'r')
+
+        # read each line and connect the approiate nodes
+        for line in file:
+            # initialize the from and to nodes
+            nFrom, nTo = line.split()
+
+            # add nodes into adjacency list if not
+            if nFrom not in self._adjList:
+                self._adjList[nFrom] = []
+            if nTo not in self._adjList:
+                self._adjList[nTo] = []
+
+            # add the edge (nFrom -> nTo)
+            self._adjList[nFrom].append(nTo)
+
+            # increment the number of edges
+            self._numEdges += 1
+        
+        # update number of nodes/vertices
+        self._numNodes = len(self._adjList)
+
+
+#-- Get the adjacent nodes
+    def get_neighbors(self, node):
+        # check if node is in adj list
+        if (node not in self._adjList):
+            return []
+        # if is, return [] of neighs
+        return self._adjList[node]
+
+
+#-- Get the number of nodes in the graph
+    def get_num_nodes(self):
+        return self._numNodes
+
+
+#-- Returns if two nodes have a direct path
+    def has_edge(self, nFrom, nTo):
+        if nFrom in self._adjList:
+            if nTo in self._adjList[nFrom]:
+                return True
+        return False
+    
+
+#-- Gets the full adjacency list
+    def get_adjList(self):
+        return self._adjList
+
