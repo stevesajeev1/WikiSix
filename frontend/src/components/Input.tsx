1dae8a57b7171593e3c391d53d44fd0c26a7bb66
sriramyy <131324693+sriramyy@users.noreply.github.com>
2025-04-16 17:43:56 -0400

    Created General Statistics Strcture

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -9,6 +9,13 @@ export default function Input() {
         <span>to</span>
         <input />
       </div>
+      <button id="find-connection-btn">Find Connection</button>
+      <div id="general-stats">
+        <h3>General Statistics</h3>
+        <p>The longest path in this dataset is <b>x to y</b>:</p>
+        <p><b>x</b> → z → c → a → j → l → y → <b>y</b></p>
+        <p>Another stat...</p>
+      </div>
     </div>
   );
 }
