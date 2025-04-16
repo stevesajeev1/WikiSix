4b6f0623e6516dfa82f59feff991ffbf2fef41e3
sriramyy <131324693+sriramyy@users.noreply.github.com>
2025-04-16 19:54:07 -0400

    Fixed the previous implementation of Dijkstra's, now returns all shortest paths

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -1,82 +1,75 @@
 # This file should have all the algorithms.
 # Each algorithm should accept a graph object, start node and end node.
 
+from collections import defaultdict
 from graph import Graph
 import heapq
 
 # Should return:
 # - Number of nodes explored (visited list length)
 # - Length/Degrees of the shortest path (optional, can just get from len(path))
-# - A path array that includes path eg. ['Florida', 'Greenland', 'Tin'] is Florida -> Greenland -> Tin
-
-# Testing Code, example using Dijkstra
-# graph = Graph("backend/data/paths.tsv")
-# print(dijkstra(graph, "Florida", "Wikipedia")) # prints (4053, 3, ['Florida', 'English_language', 'HTTP_cookie', 'Wikipedia'])
-
+# - A path array that includes all the shortest paths in a list of lists
+#   * Example [["Florida", "x", "Wikipedia"], ["Florida", "y", "Wikipedia"]]
+#   * The shortest paths are Florida -> x -> Wikipedia and Florida -> y -> Wikipedia
 
 
 # Dijkstra Algorithm - Sriram
 def dijkstra(graph: Graph, src, to):
-    # get the number of nodes in the graph
+    # get the number of nodes in the graph and initalize variables
     n = graph.get_num_nodes()
-
-    # initialize the distances and previous
     INF = float('inf')
-    dist = {node: INF for node in range(n)}
+    dist = {node: INF for node in range(n)}    
     dist[src] = 0
-    prev = {node: -1 for node in range(n)}
-    prev[src] = 0
-
-    # initialize visited set
-    visited = set()
+    prev = defaultdict(list) # prev also holds paths
 
     # priority queue to hold (node, distance)
-    pq = [(src, 0)]
+    pq = [(0, src)]
 
     # process until empty
     while pq:
-        node, distance = heapq.heappop(pq)
+        distance, node = heapq.heappop(pq)
 
-        # check if node already visited
-        if node in visited:
+        if distance > dist[node]:
             continue
         
-        # add to visited nodes
-        visited.add(node)
-
         # relax all of node's edges
         for v in graph.get_neighbors(node):
             
-            if v not in dist:
-                dist[v] = INF
+            weight = 1 # unweighted so all edges w=1
             
-            if v not in prev:
-                prev[v] = -1
-            
-            weight = 1
-            # node allows a smaller weight and v is not visited
-            if dist[v] > distance + weight:
+            # node allows a smaller weight
+            if dist.get(v, INF) > distance + weight:
                 # replace the weight, append to pq, add to visited
                 dist[v] = distance + weight
-                prev[v] = node
-                heapq.heappush(pq, (v, dist[v]))
+                prev[v] = [node]
+                heapq.heappush(pq, (dist[v], v))
             
-            if v == to:
-                # found the destination (to) node
-                break
+            elif distance+weight == dist[v]:
+                prev[v].append(node) 
 
-    # append the path
-    path = []
-    current_node = to
-    while prev[current_node] != 0 and dist[current_node] != INF:
-        path.append(current_node)
-        current_node = prev[current_node]
+    # calculate the amount of nodes visited and the least distance
+    visited = 0
+    for d in dist.values():
+        if d < INF: visited += d
+    
+    least_dist = dist[to] if dist.get(to, INF) < INF else None
 
-    path.append(src)
+    # now backtrack all the nodes for all shortest paths
+    paths = []
+    if (least_dist is not None):
+        # stack with (node, path)
+        stack = [(to, [])]
+        while stack:
+            node, path = stack.pop()
+            if node == src:
+                # reached end
+                paths.append([src] + path)
+            else:
+                # add all previous
+                for u in prev[node]:
+                    stack.append((u, [node] + path))
 
-    path.reverse()
-
-    return len(visited), dist[to] if to in dist else INF, path
+    return visited, least_dist, paths
 
 
 # breadth first search
@@ -87,3 +80,8 @@ def bfs(graph: Graph, src, to):
 # depth first search
 def dfs(graph: Graph, src, to):
     pass
+
+
+# Testing Code, example using Dijkstra
+# graph = Graph("backend/data/paths.tsv")
+# print(dijkstra(graph, "Florida", "Formula_One"))
