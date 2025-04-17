fead67d254c21ce3dd1a2a00cec728022b10a80a
Steve Sajeev <stevesajeev123@gmail.com>
2025-04-17 15:22:46 -0400

    Create footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <div id="footer">
+      <span>Made by Steve Sajeev, Sriram Yerramsetty, and Victoria Villasana for COP3530 at the University of Florida.</span>
+    </div>
+  );
+}
--- frontend/src/components/UserPath.tsx
+++ frontend/src/components/UserPath.tsx
@@ -5,19 +5,24 @@ import type { User } from "../types";
 import Path from './Path';
 
 interface UserProps {
-  user: User;
+  user: User | null;
 }
 
 export default function UserPaths({ user }: UserProps) {
   return (
     <div id="user-container">
       <span id="user-title">User Path</span>
-      <Path path={user.shortest_path} />
-      <div id="user-stats">
-        <span>The average user took <strong>{user.average_duration}</strong>s to solve.</span>
-        <span>The average degree that a user found was <strong>{user.average_length}</strong>.</span>
-        <span>The shortest degree that a user found was <strong>{user.shortest_degree}</strong>.</span>
-      </div>
+      {user === null ? 
+      <span id="no-user-routes">No user routes found!</span> :
+      <>
+        <Path path={user.shortest_path} />
+        <div id="user-stats">
+          <span>The average user took <strong>{user.average_duration}</strong>s to solve.</span>
+          <span>The average degree that a user found was <strong>{user.average_length}</strong>.</span>
+          <span>The shortest degree that a user found was <strong>{user.shortest_degree}</strong>.</span>
+        </div>
+      </>
+      }
     </div>
   )
 }
