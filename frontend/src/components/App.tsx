77330f59f2a2c0f9f9271f6895b1983318994abc
Steve Sajeev <stevesajeev123@gmail.com>
2025-04-16 23:58:59 -0400

    Structure frontend and styling

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,11 +1,21 @@
+import "../styles/App.css";
+
 import Header from './Header';
 import Input from './Input';
+import Graph from './Graph';
+import Paths from './Paths';
+import UserPaths from './UserPaths';
 
 export default function App() {
   return (
     <>
       <Header />
       <Input />
+      <Graph />
+      <div id="paths">
+        <Paths />
+        <UserPaths />
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/Paths.tsx
@@ -0,0 +1,3 @@
+export default function Paths() {
+  return <span>Individual Paths</span>;
+}
