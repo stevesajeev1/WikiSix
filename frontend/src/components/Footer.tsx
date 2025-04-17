4a1d96f8a04aaab2e7f1cd74b42c71ae4217e88b
Steve Sajeev <stevesajeev123@gmail.com>
2025-04-17 15:42:24 -0400

    Attribute dataset and add general stat

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,12 @@ import "../styles/Footer.css";
 export default function Footer() {
   return (
     <div id="footer">
-      <span>Made by Steve Sajeev, Sriram Yerramsetty, and Victoria Villasana for COP3530 at the University of Florida.</span>
+      <span id="footer-credits">Made by Steve Sajeev, Sriram Yerramsetty, and Victoria Villasana for COP3530 at the University of Florida.</span>
+      <div id="footer-info">
+        <span id="footer-info-left">Did you know that the every path is connected by only 9 clicks?</span>
+        <span>&nbsp;&bull;&nbsp;</span>
+        <a href="https://snap.stanford.edu/data/wikispeedia.html">Dataset</a>
+      </div>
     </div>
   );
 }
