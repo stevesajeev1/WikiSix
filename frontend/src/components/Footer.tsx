cac618661bd39f239ce0befaa33118508604a994
sriramyy <131324693+sriramyy@users.noreply.github.com>
2025-04-18 18:48:35 -0400

    Overhauled the Design

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,12 +2,23 @@ import "../styles/Footer.css";
 
 export default function Footer() {
   return (
-    <div id="footer">
-      <span id="footer-credits">Made by Steve Sajeev, Sriram Yerramsetty, and Victoria Villasana for COP3530 at the University of Florida.</span>
-      <div id="footer-info">
-        <span id="footer-info-left">Did you know that the every path is connected by only 9 clicks?</span>
-        <span>&nbsp;&bull;&nbsp;</span>
-        <a href="https://snap.stanford.edu/data/wikispeedia.html">Dataset</a>
+    <div>
+      <div id="general-stats">
+        <h3>Did you know?</h3>
+        <p>The longest path is between <b>Interbase</b> and <b>Timken 1111</b> with <i><b>9 degrees</b></i> of seperation!</p>
+      </div>
+
+      <div id="footer">
+        <span id="footer-credits">
+          Made by Steve Sajeev, Sriram Yerramsetty, and Victoria Villasana for COP3530 at the University of Florida.
+        </span>
+        <div id="footer-info">
+          <span id="footer-info-left">
+            4,592 articles & 119,882 links
+          </span>
+          <span>&nbsp;&bull;&nbsp;</span>
+          <a href="https://snap.stanford.edu/data/wikispeedia.html">Dataset</a>
+        </div>
       </div>
     </div>
   );
